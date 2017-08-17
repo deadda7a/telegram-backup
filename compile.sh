d68cc742beee560afc6a753f7bae578e3178df0f
Sebastian Elisa Pfeifer <sebastian.pfeifer@unicorncloud.org>
2017-08-17 22:13:01 +0200

    also deploy for debian jessie

--- compile.sh
+++ compile.sh
@@ -4,10 +4,10 @@ cd tg
 ./configure --disable-openssl
 make
 cd ..
-#git clone --recursive https://github.com/tvdstaaij/telegram-history-dump.git
 mv tg/bin/telegram-cli package/usr/bin/
-chown -R root:root package
+#chown -R root:root package
 dpkg-deb --build package
 mv package.deb telegram-cli.deb
+cp telegram-cli.deb telegram-cli-jessie.deb
 package_cloud push deadda7a/telegram-cli/ubuntu/xenial ./telegram-cli.deb
-package_cloud push deadda7a/telegram-cli/debian/jessie ./telegram-cli.deb
+package_cloud push deadda7a/telegram-cli/debian/jessie ./telegram-cli-jessie.deb
