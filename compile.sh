61c4867e3570b131ec6787a5fbdd9884d8f6bd61
Sebastian Elisa Pfeifer <sebastian.pfeifer@unicorncloud.org>
2017-08-17 18:49:53 +0200

    first draft

--- compile.sh
+++ compile.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 git clone --recursive https://github.com/vysheng/tg.git
 cd tg
-sudo apt-get install libreadline-dev libconfig-dev libssl-dev lua5.2 liblua5.2-dev libevent-dev libjansson-dev libpython-dev make
 ./configure --disable-openssl
 make
 cd ..
-git clone --recursive https://github.com/tvdstaaij/telegram-history-dump.git
+#git clone --recursive https://github.com/tvdstaaij/telegram-history-dump.git
+mv tg/bin/telegram-cli package/usr/bin/
+chown -R root:root package
+dpkg-deb --build package
